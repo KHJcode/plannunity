df2704d45fc161b308b769692aaf7767dd7ef7cc
Na jewon <80616120+jewonr@users.noreply.github.com>
2022-12-21 12:59:12 +0900

    fix import expression

--- src/modules/cart.ts
+++ src/modules/cart.ts
@@ -1,20 +1,18 @@
-import { PlanData } from "../../pages";
-
 const LOAD_CART = 'LOAD_CART' as const;
 
-export const loadCart = (plans: PlanData[]) => ({
+export const loadCart = (plans: any) => ({
   type: LOAD_CART,
   payload: plans,
 });
 
 export type CartAction = ReturnType<typeof loadCart>;
 
-const initialState: PlanData[] = [];
+const initialState: any = [];
 
 export default function cart(
-  state: PlanData[] = initialState,
+  state: any = initialState,
   action: CartAction
-): PlanData[] {
+): any {
   switch(action.type) {
     case LOAD_CART:
       return action.payload;
